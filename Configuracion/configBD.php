d6a731704f562a295d4868151bf507871e02d726
Obed Martinez <martinezobednoe@gmail.com>
2021-06-19 15:40:19 -0500

    Framework funcionando con rutas, eloquent y jwt

--- Configuracion/configBD.php
+++ Configuracion/configBD.php
@@ -2,8 +2,8 @@
 require('vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable('./');
 $dotenv->load();
-defined("DBDRIVER")or define('DBDRIVER',$_ENV['DBDriver']);
-defined("DBHOST")or define('DBHOST',$_ENV['DBHOST']);
-defined("DBNAME")or define('DBNAME',$_ENV['DBName']);
-defined("DBUSER")or define('DBUSER',$_ENV['DBUser']);
-defined("DBPASS")or define('DBPASS',$_ENV['DBPass']);
+defined("DBDRIVER") or define('DBDRIVER', $_ENV['DBDriver']);
+defined("DBHOST") or define('DBHOST', $_ENV['DBHOST']);
+defined("DBNAME") or define('DBNAME', $_ENV['DBName']);
+defined("DBUSER") or define('DBUSER', $_ENV['DBUser']);
+defined("DBPASS") or define('DBPASS', $_ENV['DBPass']);
