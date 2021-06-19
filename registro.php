65bf427a493c13f41a7a416e34464aade4594cd0
Obed Martinez <martinezobednoe@gmail.com>
2021-06-19 00:40:04 -0500

    Se incluyo validador

--- registro.php
+++ registro.php
@@ -2,15 +2,27 @@
 require('Modelos/DB.php');
 require("Modelos/usuario.php");
 require("JWT/config.php");
+use Rakit\Validation\Validator;
 require("Modelos/configuracionHttps.php");
 date_default_timezone_set('America/Mexico_City');
 $data = json_decode(file_get_contents('php://input'), true);
 if (!isset($data)) {
     $data = $_POST;
 }
-$username = $data['username'];
-$password = $data['password'];
-$mail = $data['mail'];
+$validator = new Validator;
+$validation = $validator->make($data, [
+    'apellidos'                  => 'required',
+    'nombre'                  => 'required',
+    'username'                  => 'required',
+    'mail'                 => 'required|email',
+    'password'              => 'required|min:8',
+    'rpassword'      => 'required|same:password',
+]);
+$validation->validate();
+if ($validation->fails()) {
+    echo json_encode(["estado"=>false,"detalle"=>$errors = $validation->errors()]);
+    return;
+}
 $db = new BaseDatos();
 $db = $db->conectar();
 $select = $db->prepare("Select * from usuario where `username`=? OR email=?");
@@ -24,14 +36,13 @@ if (isset($usuario)) {
 } else {
     $JWT = new Auth();
     $insert = $db->prepare("Insert into usuario values(null,?,?,?,?,?)");
-    $password = password_hash($password, PASSWORD_DEFAULT);
+    $password = password_hash($data['password'], PASSWORD_DEFAULT);
     $momento = date('c');
-    $insert->bind_param("sssss", $username, $mail, $password, $momento, $momento);
-
+    $insert->bind_param("sssss", $data['username'], $data['mail'], $password, $momento, $momento);
     $insert->execute();
 
     $select = $db->prepare("Select * from usuario where `username`=? OR email=?");
-    $select->bind_param("ss", $username, $mail);
+    $select->bind_param("ss", $data['username'], $data['mail']);
     $select->execute();
     $result = $select->get_result();
     $usuario = $result->fetch_object(Usuario::class);
