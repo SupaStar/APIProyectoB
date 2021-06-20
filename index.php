8577fbc064bb42647813d1847152ac59efda5394
Obed Martinez <martinezobednoe@gmail.com>
2021-06-19 23:26:20 -0500

    Prueba cors

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 require 'start.php';
 require('Configuracion/JWT/Middleware.php');
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
 use Controllers\ApiController;
 use Controllers\UsuarioController;
 use Middleware\MiddlewareJwt;
